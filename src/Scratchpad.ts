8118f240b8e9bec2045c5f7894467d7e9f828935
Marian Vanderka <vanderka.marian@gmail.com>
2018-06-13 11:07:40 +0200

    support lists

--- src/Scratchpad.ts
+++ src/Scratchpad.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 import { deserialize, plainToClass, serialize } from "class-transformer";
 import "reflect-metadata";
+import { Observable } from "rxjs/internal/Observable";
 import { Account } from "./models/Account";
 import { Asset } from "./models/Asset";
 import { Authority } from "./models/Authority";
@@ -83,7 +84,7 @@ function some() {
 
 function accountByName() {
     const api = new RpcEndpoints();
-    api.getAccountByName(new GetAccountByName("u961279ec8b7ae7bd62f304f7c1c3d345")).subscribe(
+    api.makeRequest(new GetAccountByName("u961279ec8b7ae7bd62f304f7c1c3d345")).subscribe(
         (account) => console.log(account),
         (err) => console.error(err)
     )
@@ -91,14 +92,45 @@ function accountByName() {
 
 function accountById() {
     const api = new RpcEndpoints();
-    api.makeRequest(new GetAccountById(ChainObject.parse("1.2.15"))).subscribe(
+    return api.makeRequest(new GetAccountById(ChainObject.parse("1.2.15"))).subscribe(
         (account) => console.log(account),
         (err) => console.error(err)
     )
 }
 
 function serialize_account() {
-    const json = {"id":"1.2.34","registrar":"1.2.15","name":"u961279ec8b7ae7bd62f304f7c1c3d345","owner":{"weight_threshold":1,"account_auths":[],"key_auths":[["DCT6MA5TQQ6UbMyMaLPmPXE2Syh5G3ZVhv5SbFedqLPqdFChSeqTz",1]]},"active":{"weight_threshold":1,"account_auths":[],"key_auths":[["DCT6MA5TQQ6UbMyMaLPmPXE2Syh5G3ZVhv5SbFedqLPqdFChSeqTz",1]]},"options":{"memo_key":"DCT6MA5TQQ6UbMyMaLPmPXE2Syh5G3ZVhv5SbFedqLPqdFChSeqTz","voting_account":"1.2.3","num_miner":0,"votes":[],"extensions":[],"allow_subscription":false,"price_per_subscribe":{"amount":0,"asset_id":"1.3.0"},"subscription_period":0},"rights_to_publish":{"is_publishing_manager":false,"publishing_rights_received":[],"publishing_rights_forwarded":[]},"statistics":"2.5.34","top_n_control_flags":0}
+    const json = {
+        "id": "1.2.34",
+        "registrar": "1.2.15",
+        "name": "u961279ec8b7ae7bd62f304f7c1c3d345",
+        "owner": {
+            "weight_threshold": 1,
+            "account_auths": [],
+            "key_auths": [["DCT6MA5TQQ6UbMyMaLPmPXE2Syh5G3ZVhv5SbFedqLPqdFChSeqTz", 1]]
+        },
+        "active": {
+            "weight_threshold": 1,
+            "account_auths": [],
+            "key_auths": [["DCT6MA5TQQ6UbMyMaLPmPXE2Syh5G3ZVhv5SbFedqLPqdFChSeqTz", 1]]
+        },
+        "options": {
+            "memo_key": "DCT6MA5TQQ6UbMyMaLPmPXE2Syh5G3ZVhv5SbFedqLPqdFChSeqTz",
+            "voting_account": "1.2.3",
+            "num_miner": 0,
+            "votes": [],
+            "extensions": [],
+            "allow_subscription": false,
+            "price_per_subscribe": { "amount": 0, "asset_id": "1.3.0" },
+            "subscription_period": 0
+        },
+        "rights_to_publish": {
+            "is_publishing_manager": false,
+            "publishing_rights_received": [],
+            "publishing_rights_forwarded": []
+        },
+        "statistics": "2.5.34",
+        "top_n_control_flags": 0
+    }
     const acc = plainToClass(Account, json);
     console.log(acc.owner.keyAuths)
 }
@@ -106,3 +138,4 @@ function serialize_account() {
 // some();
 // serialize_account()
 accountById()
+// accountByName()
--- src/net/rpc/RpcEndpoints.ts
+++ src/net/rpc/RpcEndpoints.ts
@@ -13,7 +13,7 @@ export class RpcEndpoints {
         timeout: 15000,
     });
 
-    public makeRequest<T>(request: BaseRequest<T>): Observable<T> {
+    public makeRequest<T>(request: BaseRequest<T>): Observable<T | T[]> {
         return this.baseRequest.post("", { body: serialize(request) }).pipe(
             map((data) => {
                 if (data.response.statusCode === 200) {
@@ -21,7 +21,8 @@ export class RpcEndpoints {
                     if (!_.isNil(response.error)) {
                         throw Error(response.error.message);
                     }
-                    if (!_.isNil(response.result)) {
+                    if (_.isObject(response.result)
+                        && (!_.isArray(response.result) || response.result.filter(Boolean).length > 0)) {
                         return plainToClass(request.returnClass, response.result);
                     }
                     throw new NotFoundError(request.description());
