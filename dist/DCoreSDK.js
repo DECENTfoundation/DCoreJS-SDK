c0c293acc03a52f74f42ce21052b0a35d139a792
Marian Vanderka <vanderka.marian@gmail.com>
2018-07-11 13:22:07 +0200

    removed dist, update BuyContentOperation name

--- dist/DCoreSDK.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class DCoreSDK {
-    getBalance(accountId) {
-        return null;
-    }
-}
-exports.DCoreSDK = DCoreSDK;
-//# sourceMappingURL=DCoreSDK.js.map
