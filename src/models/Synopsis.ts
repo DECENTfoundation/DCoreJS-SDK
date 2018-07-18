591625c6d30a85b23898016dcdb8a8b0c996bc2b
Marian Vanderka <vanderka.marian@gmail.com>
2018-07-18 15:09:02 +0200

    authority associated array, synopsis removed invalid attributes

--- src/models/Synopsis.ts
+++ src/models/Synopsis.ts
@@ -11,14 +11,4 @@ export class Synopsis {
     @Type(() => ChainObject)
     @Expose({ name: "content_type_id" })
     public type: ChainObject;
-
-    @Expose({ name: "file_name" })
-    public fileName?: string;
-
-    @Expose({ name: "sampleURL" })
-    public sampleUrl?: string;
-
-    @Expose({ name: "fileFormat" })
-    public fileFormat?: string;
-
 }
