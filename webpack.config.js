cc6f761b26941a8ed78027d78d429c0f615f5cc9
Michal Grman <mike@decent.ch>
2018-09-06 21:36:41 +0200

    Updated config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./src/DCoreSDK.ts",
-    output: {
-      filename: "bundle.js"
-    },
-    resolve: {
-      // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: "ts-loader" }
-      ]
-    }
-  };
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js"
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
