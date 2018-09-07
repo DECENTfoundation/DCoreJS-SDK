2fded8b265f93ab78665aaad8866e1c80eeedd72
Marian Vanderka <vanderka.marian@gmail.com>
2018-09-07 13:39:08 +0200

    added index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-// export * from "./DCoreApi";
-// export * from "./DCoreApiRxImpl";
+// created from "create-ts-index"
+
+export * from "./api";
+export * from "./crypto";
+export * from "./models";
+export * from "./utils";
+export * from "./DCoreApi";
+export * from "./DCoreApiRxImpl";
 export * from "./DCoreSdk";
