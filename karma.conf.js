63de4e0c337049c0c4b0f282ff41cdaddb071150
gmalicky <34336799+gmalicky@users.noreply.github.com>
2019-03-26 17:31:52 +0100

    Add karma setup

---
+++ karma.conf.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ["mocha", "karma-typescript"],
+        files: [
+            "test/**/*1.ts", // *.tsx for React Jsx
+            "src/**/*.ts"
+        ],
+        preprocessors: {
+            "**/*.ts": "karma-typescript" // *.tsx for React Jsx
+        },
+        reporters: ["mocha", "karma-typescript"],
+        browsers: ["Chrome"],
+        singleRun: true
+    });
+};
