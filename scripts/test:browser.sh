fb0f78336aad6a433c71f8352ac770ce75c02040
Peter Vanderka <peter.vanderka@decent.ch>
2019-06-12 16:04:21 +0200

    Separate DCore, speed up tests

--- scripts/test:browser.sh
+++ scripts/test:browser.sh
@@ -1,1 +1,3 @@
+docker-compose -f ./test-dcore-node/docker-compose.yml up -d --build
 karma start
+docker-compose -f ./test-dcore-node/docker-compose.yml down
--- scripts/test:node.sh
+++ scripts/test:node.sh
@@ -1,1 +1,3 @@
+docker-compose -f ./test-dcore-node/docker-compose.yml up -d --build
 mocha 'test/**/*.test.ts'
+docker-compose -f ./test-dcore-node/docker-compose.yml down
